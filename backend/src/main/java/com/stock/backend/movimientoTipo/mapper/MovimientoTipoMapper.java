6d82c738e9c2261f70e1edcb38f6fe06932183e1
bdidominguez <bdidominguez@gmail.com>
2026-02-22 21:42:27 -0300

    agregando mapstrusct

--- backend/src/main/java/com/stock/backend/movimientoTipo/mapper/MovimientoTipoMapper.java
+++ backend/src/main/java/com/stock/backend/movimientoTipo/mapper/MovimientoTipoMapper.java
@@ -1,32 +1,14 @@
 package com.stock.backend.movimientoTipo.mapper;
 
+import org.mapstruct.Mapper;
+
 import com.stock.backend.movimientoTipo.dto.MovimientoTipoDTO;
 import com.stock.backend.movimientoTipo.entity.MovimientoTipo;
 
-public class MovimientoTipoMapper {
+@Mapper(componentModel = "spring")
+public interface MovimientoTipoMapper {
 
-    public static MovimientoTipoDTO toDto(MovimientoTipo mov){
-        return new MovimientoTipoDTO(
-            mov.getId(),
-            mov.getSiglas(),
-            mov.getNombre(),
-            mov.getDescripcion(),
-            mov.getAfectaStock(),
-            mov.getRequiereSucursalOrigen(),
-            mov.getRequiereSucursalDestino(),
-            mov.getActivo()
-        );
-    }
+    MovimientoTipoDTO toDto(MovimientoTipo mov);
 
-    public static MovimientoTipo toEntidad(MovimientoTipoDTO dto){
-        return new MovimientoTipo(
-            dto.id(),
-            dto.siglas(),
-            dto.nombre(),
-            dto.descripcion(),
-            dto.afectaStock(),
-            dto.requiereSucursalOrigen(),
-            dto.requiereSucursalDestino(),
-            dto.activo());
-    }
+    MovimientoTipo toEntidad(MovimientoTipoDTO dto);
 }
