6c90f044d633f738012c9fbae3ee55c7a227bcda
bdidominguez <bdidominguez@gmail.com>
2026-02-26 14:16:13 -0300

    aplicando mapstruct

--- backend/src/main/java/com/stock/backend/categoria/mapper/CategoriaMapper.java
+++ backend/src/main/java/com/stock/backend/categoria/mapper/CategoriaMapper.java
@@ -1,23 +1,14 @@
 package com.stock.backend.categoria.mapper;
 
+import org.mapstruct.Mapper;
+
 import com.stock.backend.categoria.dto.CategoriaDTO;
 import com.stock.backend.categoria.entity.Categoria;
 
-public class CategoriaMapper {
+@Mapper(componentModel="spring")
+public interface CategoriaMapper {
 
-    public static CategoriaDTO toDto(Categoria cat){
-        return new CategoriaDTO(
-                cat.getId(),
-                cat.getCodigo(),
-                cat.getNombre(),
-                cat.getDescripcion(),
-                cat.getActivo(),
-                cat.getFechaCreacion(),
-                cat.getFechaModificacion()
-        );
-    }
+    CategoriaDTO toDto(Categoria cat);
 
-    public static Categoria toEntidad(CategoriaDTO dto){
-        return new Categoria(dto.id(), dto.codigo(), dto.nombre(), dto.descripcion(), dto.activo(), dto.fechaCreacion(), dto.fechaModificacion() );
-    }
+    Categoria toEntidad(CategoriaDTO dto);
 }
--- backend/src/main/java/com/stock/backend/categoria/mapper/CategoriaNuevaMapper.java
+++ backend/src/main/java/com/stock/backend/categoria/mapper/CategoriaNuevaMapper.java
@@ -1,16 +1,15 @@
 package com.stock.backend.categoria.mapper;
 
+import org.mapstruct.Mapper;
+
 import com.stock.backend.categoria.dto.CategoriaNuevaDTO;
 import com.stock.backend.categoria.entity.Categoria;
 
-public class CategoriaNuevaMapper {
+@Mapper(componentModel = "spring")
+public interface CategoriaNuevaMapper {
 
-    public static Categoria toEntidad(CategoriaNuevaDTO cat){
-        return new Categoria(null, cat.codigo(), cat.nombre(), cat.descripcion(), cat.activo(), cat.fechaCreacion(), cat.fechaModificacion());
-    }
+    Categoria toEntidad(CategoriaNuevaDTO cat);
 
-    public static CategoriaNuevaDTO toDTO(Categoria dto){
-        return new CategoriaNuevaDTO(dto.getCodigo(), dto.getNombre(), dto.getDescripcion(), dto.getActivo(), dto.getFechaCreacion(), dto.getFechaModificacion());
-    }
+    CategoriaNuevaDTO toDTO(Categoria dto);
 
 }
